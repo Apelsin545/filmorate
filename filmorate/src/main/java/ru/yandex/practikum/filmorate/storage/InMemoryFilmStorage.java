6561afd6b21c000a54689e5a4061b0130364b7c1
Apelsin545 <dmitri_ch@list.ru>
2024-03-11 20:13:29 +0300

    feat: добавить InMemoryFilmStorage, InMemoryUserStorage

---
+++ filmorate/src/main/java/ru/yandex/practikum/filmorate/storage/InMemoryFilmStorage.java
@@ -0,0 +1,20 @@
+package ru.yandex.practikum.filmorate.storage;
+
+import ru.yandex.practikum.filmorate.model.Film;
+
+public class InMemoryFilmStorage implements FilmStorage{
+    @Override
+    public Film add(Film film) {
+        return null;
+    }
+
+    @Override
+    public Film remove(Film film) {
+        return null;
+    }
+
+    @Override
+    public Film update(Film film) {
+        return null;
+    }
+}
---
+++ filmorate/src/main/java/ru/yandex/practikum/filmorate/storage/InMemoryUserStorage.java
@@ -0,0 +1,20 @@
+package ru.yandex.practikum.filmorate.storage;
+
+import ru.yandex.practikum.filmorate.model.User;
+
+public class InMemoryUserStorage implements UserStorage {
+    @Override
+    public User add(User user) {
+        return null;
+    }
+
+    @Override
+    public User remove(User user) {
+        return null;
+    }
+
+    @Override
+    public User update(User user) {
+        return null;
+    }
+}
