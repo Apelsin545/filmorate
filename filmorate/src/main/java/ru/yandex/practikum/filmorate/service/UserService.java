1344e1cb457b2cf5966854e335f7b46a1d3d24ee
Apelsin545 <dmitri_ch@list.ru>
2024-03-11 22:13:56 +0300

    feat: добавить метод addFriend() в UserService

--- filmorate/src/main/java/ru/yandex/practikum/filmorate/service/UserService.java
+++ filmorate/src/main/java/ru/yandex/practikum/filmorate/service/UserService.java
@@ -2,6 +2,7 @@ package ru.yandex.practikum.filmorate.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practikum.filmorate.model.User;
 import ru.yandex.practikum.filmorate.storage.InMemoryUserStorage;
 
 @Service
@@ -11,6 +12,13 @@ public class UserService {
     @Autowired
     public UserService(InMemoryUserStorage users) {
         this.users = users;
+    }
+
+    public boolean addFriend(User user, User anotherUser) {
+        if (anotherUser == null) return false;
+        user.getFriends().add(anotherUser.getId());
+        
+        return true;
     }
 
 
