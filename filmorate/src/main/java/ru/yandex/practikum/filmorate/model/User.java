85b7c08916f7570ae4e6cdef3652c54d90285124
Apelsin545 <dmitri_ch@list.ru>
2024-03-14 21:10:08 +0300

    feat: изменить поле friends в User, для подтверждения добавления в друзья

--- filmorate/src/main/java/ru/yandex/practikum/filmorate/model/User.java
+++ filmorate/src/main/java/ru/yandex/practikum/filmorate/model/User.java
@@ -1,25 +1,21 @@
 package ru.yandex.practikum.filmorate.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NonNull;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
 public class User {
     private final int id;
 
-    private final Set<Integer> friends = new HashSet<>();
+    private final Map<Integer, String> friends = new HashMap<>();
 
     @Email
     private String email;
