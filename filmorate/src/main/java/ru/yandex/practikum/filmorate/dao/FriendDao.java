f72af7335441a1416a5a6391bfe353a9e0e67865
Apelsin545 <dmitri_ch@list.ru>
2024-03-21 22:58:32 +0300

    feat: создать интерфейс FriendDao

---
+++ filmorate/src/main/java/ru/yandex/practikum/filmorate/dao/FriendDao.java
@@ -0,0 +1,16 @@
+package ru.yandex.practikum.filmorate.dao;
+
+import ru.yandex.practikum.filmorate.model.User;
+
+import java.util.List;
+
+public interface FriendDao {
+    List<User> getFriends(int userId);
+
+    void sendRequest(int userFrom, int userTo);
+    void rejectRequest(int userFrom, int userTo);
+
+    void removeFriend(int userFrom, int userToRemove);
+
+    void acceptRequest(int userFrom, int userTo);
+}
