1c2e7d19a9e39b712fe43c867351039fcd27dfc0
Apelsin545 <dmitri_ch@list.ru>
2024-03-22 11:38:13 +0300

    feat: реализовать getPopularFilms() в FilmLikeDaoImpl

--- filmorate/src/main/java/ru/yandex/practikum/filmorate/dao/Impl/FilmLikeDaoImpl.java
+++ filmorate/src/main/java/ru/yandex/practikum/filmorate/dao/Impl/FilmLikeDaoImpl.java
@@ -3,19 +3,25 @@ package ru.yandex.practikum.filmorate.dao.Impl;
 import org.apache.tomcat.Jar;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
+import ru.yandex.practikum.filmorate.dao.FilmDao;
 import ru.yandex.practikum.filmorate.dao.FilmLikeDao;
 import ru.yandex.practikum.filmorate.model.Film;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Component
 public class FilmLikeDaoImpl implements FilmLikeDao {
     private final JdbcTemplate jdbcTemplate;
+    private final FilmDao filmDao;
 
     @Autowired
-    public FilmLikeDaoImpl(JdbcTemplate jdbcTemplate) {
+    public FilmLikeDaoImpl(JdbcTemplate jdbcTemplate, FilmDao filmDao) {
         this.jdbcTemplate = jdbcTemplate;
+        this.filmDao = filmDao;
     }
 
     @Override
@@ -30,6 +36,14 @@ public class FilmLikeDaoImpl implements FilmLikeDao {
 
     @Override
     public List<Film> getPopularFilms(int max) {
-        return null;
+        String sql = "select film_id, count(film_id) from filmorate_film_like group by film_id order by count(film_id) desc";
+        List<Film> popularFilms = new ArrayList<>();
+
+        SqlRowSet films = jdbcTemplate.queryForRowSet(sql);
+        for (int i = 0; i < max && films.next(); i++) {
+            popularFilms.add(filmDao.getFilmById(Integer.parseInt(Objects.requireNonNull(films.getString("film_id")))));
+        }
+
+        return popularFilms;
     }
 }
