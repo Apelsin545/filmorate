ea8a38d9b07e91daccbda4a894927bf8979d9391
Apelsin545 <dmitri_ch@list.ru>
2024-03-22 12:42:05 +0300

    feat: реализовать методы в UserService

--- filmorate/src/main/java/ru/yandex/practikum/filmorate/service/UserService.java
+++ filmorate/src/main/java/ru/yandex/practikum/filmorate/service/UserService.java
@@ -2,6 +2,7 @@ package ru.yandex.practikum.filmorate.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practikum.filmorate.dao.FriendDao;
 import ru.yandex.practikum.filmorate.dao.Impl.UserDaoImpl;
 import ru.yandex.practikum.filmorate.dao.UserDao;
 import ru.yandex.practikum.filmorate.model.User;
@@ -14,13 +15,39 @@ import java.util.stream.Collectors;
 @Service
 public class UserService {
     private final UserDao userDao;
+    private final FriendDao friendDao;
 
     @Autowired
-    public UserService(UserDao userDao) {
+    public UserService(UserDao userDao, FriendDao friendDao) {
         this.userDao = userDao;
+        this.friendDao = friendDao;
     }
 
     public User getUserById(int id) {
         return userDao.getUserById(id);
+    }
+
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    public void createUser(User user) {
+        userDao.createUser(user);
+    }
+
+    public void removeUser(int userId) {
+        userDao.removeUser(userId);
+    }
+
+    public List<User> getFriends(int userId) {
+        return friendDao.getFriends(userId);
+    }
+
+    public void addFriend(int userFromId, int userToId) {
+        friendDao.sendRequest(userFromId, userToId);
+    }
+
+    public void deleteFriend(int userFromId, int userToId) {
+        friendDao.rejectRequest(userFromId, userToId);
     }
 }
